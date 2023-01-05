31b01b3209acdd5a18ad319252a1df0b918633c7
Jing0717 <bleach81618@gmail.com>
2023-01-05 16:53:22 +0800

    [feat] add dev/prod environment error message handling

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
@@ -10,6 +9,13 @@ var usersRouter = require('./routes/users');
 require('./connections')
 
 var app = express();
+// 程式出現重大錯誤時
+process.on('uncaughtException', (err) => {
+  // 記錄錯誤下來，等到服務都處理完後，停掉該 process
+  console.error('Uncaught Exception！');
+  console.error(err);
+  process.exit(1);
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,20 +30,49 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+// express 錯誤處理:
+// *自己設定的 err 錯誤
+const resErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      message: err.message,
+    });
+  } else {
+    // log 紀錄
+    console.error('出現重大錯誤', err);
+    // 送出罐頭預設訊息
+    res.status(500).json({
+      status: 'error',
+      message: '系統錯誤，請恰系統管理員',
+    });
+  }
+};
+// *開發環境錯誤
+const resErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    message: err.message,
+    error: err,
+    stack: err.stack,
+  });
+};
+// 錯誤處理
+app.use(function (err, req, res) {
+  err.statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV === 'dev') {
+    return resErrorDev(err, res);
+  }
+  // production
+  if (err.name === 'ValidationError') {
+    err.message = '資料欄位未填寫正確，請重新輸入！';
+    err.isOperational = true;
+    return resErrorProd(err, res);
+  }
+  resErrorProd(err, res);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 未捕捉到的 catch
+process.on('unhandledRejection', (err, promise) => {
+  console.error('未捕捉到的 rejection：', promise, '原因：', err);
 });
 
 module.exports = app;
